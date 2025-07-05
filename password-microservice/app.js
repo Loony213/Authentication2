6670c96d01a3c648cd67fe00e57df03d074e5101
Loony213 <150311939+Loony213@users.noreply.github.com>
2025-07-04 23:31:11 -0500

    new code

--- password-microservice/app.js
+++ password-microservice/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const cors = require('cors');  
 const PasswordController = require('./controllers/passwordController');
 const config = require('./config/config');
 
 const app = express();
 
-// Ruta para la generación de contraseñas
+app.use(cors());
+
 app.get('/generate-password', PasswordController.generatePassword);
 
-// Iniciamos el servidor
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
 });
