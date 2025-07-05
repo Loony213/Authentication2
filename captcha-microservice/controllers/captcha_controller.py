6f9d91d56641341dd887fe31d7da343f8b70c5a1
Loony213 <150311939+Loony213@users.noreply.github.com>
2025-07-05 11:28:44 -0500

    fix bugs

--- captcha-microservice/controllers/captcha_controller.py
+++ captcha-microservice/controllers/captcha_controller.py
@@ -6,18 +6,20 @@ import io
 app = Flask(__name__)
 
 # Habilitar CORS para todas las rutas
-CORS(app, resources={r"/*": {"origins": "*"}})  # Esto permite solicitudes desde cualquier origen
+CORS(app, resources={r"/*": {"origins": "*"}})  # Permitir solicitudes desde cualquier origen
 
 @app.route('/generate-captcha', methods=['GET'])
 def generate_captcha():
     """Genera un captcha y lo devuelve como imagen."""
     captcha_text, captcha_image = CaptchaService.generate_captcha()
-    
+
     # Guardar la imagen en memoria
     img_io = io.BytesIO()
-    captcha_image.write(captcha_text, img_io)
-    img_io.seek(0)
-    
+
+    # En lugar de pasar captcha_text como argumento, simplemente escribe la imagen
+    captcha_image.save(img_io, 'PNG')  # Usa el método `save()` para guardar la imagen en img_io
+    img_io.seek(0)  # Regresa al inicio del flujo de bytes para enviarlo como respuesta
+
     # Enviar la imagen como respuesta HTTP
     return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='captcha.png')
 
